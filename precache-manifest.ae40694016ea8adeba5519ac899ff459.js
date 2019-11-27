616244c29dee040dfecb03020bdc8722aeaa7896
Noah Sylwester <nanoabram@comcast.net>
2019-11-27 14:22:42 -0800

    Updates

---
+++ precache-manifest.ae40694016ea8adeba5519ac899ff459.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e99e136e6a80d5fe26e6202885293cf",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "c2b37f3283bc603d3b07",
+    "url": "/ClickyGame/static/css/main.9d3fae2d.chunk.css"
+  },
+  {
+    "revision": "1bb1122c1c91127beaea",
+    "url": "/ClickyGame/static/js/2.be267a95.chunk.js"
+  },
+  {
+    "revision": "c2b37f3283bc603d3b07",
+    "url": "/ClickyGame/static/js/main.21279eaa.chunk.js"
+  },
+  {
+    "revision": "c103f6e8abe656d9b70b",
+    "url": "/ClickyGame/static/js/runtime-main.c0446f0b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ClickyGame/static/media/logo.25bf045c.svg"
+  }
+]);
